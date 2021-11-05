9890ebb8c798bf8db7bef9332b4ac8c1a5bfcce1
Jeffrey Zhou <jingzhou@microsoft.com>
2021-11-04 20:32:43 -0700

    Fix Duplicate entry '1' for key 'accounts_token.user_id' issue

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -3,14 +3,17 @@ import pytz
 from django.utils import timezone
 
 def custom_create_token(token_model, user, serializer):
-    token = token_model.objects.create(user=user)
-    utc_now = timezone.now()
-    utc_now = utc_now.replace(tzinfo=pytz.utc)
-    token.created = utc_now
-    token.updated = utc_now
-    token.expired = utc_now
-    token.create_time = token.created.timestamp()
-    token.updated_time = token.updated.timestamp()
-    token.expire_time = token.expired.timestamp()
-    token.save()
+    utc_now = timezone.now().replace(tzinfo=pytz.utc)
+    # create token if not exist or get it if it exists
+    # created indicates whether it's newly created
+    # reference : https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication
+    token, created = token_model.objects.get_or_create(user=user)
+    if created:
+        token.created = utc_now
+        token.updated = utc_now
+        token.expired = utc_now
+        token.create_time = token.created.timestamp()
+        token.update_time = token.updated.timestamp()
+        token.expire_time = token.expired.timestamp()
+        token.save()
     return token
